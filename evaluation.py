1941972a04a44ecbaad780bf16763737341ecd6d
YNedderhoff <yanicknedderhoff@gmail.com>
2015-04-20 16:08:08 +0200

    featurevector

--- evaluation.py
+++ evaluation.py
@@ -5,7 +5,22 @@ class Token(object):
 	def __init__( self, line ):
 		entries = line.split('\t')
 		self.form = entries[0]
-		self.pos = entries[1]
+		self.gold_pos = entries[1]
+		self.predicted_pos = entries [3]
+
+	def createFeatureVector(self, featvec, currentToken, previousToken, nextToken):
+		self.sparseFeatvec = {}
+		#print len(featvec)
+		
+		self.sparseFeatvec[featvec["current_pos_"+str(currentToken.gold_pos)]] = 1
+		if previousToken: self.sparseFeatvec[featvec["prev_pos_"+str(previousToken.gold_pos)]] = 1
+		if nextToken: self.sparseFeatvec[featvec["next_pos_"+str(nextToken.gold_pos)]] = 1
+
+		self.sparseFeatvec[featvec["current_form_"+str(currentToken.form)]] = 1
+		if previousToken: self.sparseFeatvec[featvec["prev_form_"+str(previousToken.form)]] = 1
+		if nextToken: self.sparseFeatvec[featvec["next_form_"+str(nextToken.form)]] = 1
+		#for feat in featvec:
+			
 
 
 def sentences( filestream ):
@@ -112,33 +127,56 @@ def evaluate(posDict):
 	for pos in uniqueTagsScores.keys():
 		print pos+"\tAccuracy: "+str(uniqueTagsScores[pos]['Accuracy'])+"\tPrecision: "+str(uniqueTagsScores[pos]['Precision'])+"\tRecall: "+str(uniqueTagsScores[pos]['Recall'])+"\tF-Score: "+str(uniqueTagsScores[pos]['F-Score'])
 
+def extractFeatures(filein):
+
+	featvec = {}
+	for sid, sentence in enumerate(sentences(codecs.open(filein,encoding='utf-8'))):
+		for tid,token in enumerate(sentence):
+
+			# pos features
+
+			if not "current_pos_"+str(token.gold_pos) in featvec: featvec["current_pos_"+str(token.gold_pos)] = len(featvec.keys())
+			if not "current_pos_"+str(token.predicted_pos) in featvec: featvec["current_pos_"+str(token.predicted_pos)] = len(featvec.keys())
+			if not "prev_pos_"+str(token.gold_pos) in featvec: featvec["prev_pos_"+str(token.gold_pos)] = len(featvec.keys())
+			if not "prev_pos_"+str(token.predicted_pos) in featvec: featvec["prev_pos_"+str(token.predicted_pos)] = len(featvec.keys())
+			if not "next_pos_"+str(token.gold_pos) in featvec: featvec["next_pos_"+str(token.gold_pos)] = len(featvec.keys())
+			if not "next_pos_"+str(token.predicted_pos) in featvec: featvec["next_pos_"+str(token.predicted_pos)] = len(featvec.keys())
+
+			# form features
+
+			if not "current_form_"+str(token.form) in featvec: featvec["current_form_"+str(token.form)] = len(featvec.keys())
+			if not "prev_form_"+str(token.form) in featvec: featvec["prev_form_"+str(token.form)] = len(featvec.keys())
+			if not "next_form_"+str(token.form) in featvec: featvec["next_form_"+str(token.form)] = len(featvec.keys())
+
+	return featvec
 
 if __name__=='__main__':
 	t0 = time.time()
 	import argparse
 	argpar = argparse.ArgumentParser(description='Creates a feature representation for each word in a given file in CoNLL09 format')
 	
-	argpar.add_argument('-g','--gold',dest='gold',help='gold',required=True)
-	argpar.add_argument('-p','--prediction',dest='prediction',help='prediction',required=True)
+	argpar.add_argument('-i','--infile',dest='infile',help='infile',required=True)
+	#argpar.add_argument('-g','--gold',dest='gold',help='gold',required=True)
+	#argpar.add_argument('-p','--prediction',dest='prediction',help='prediction',required=True)
 	#argpar.add_argument('-o','--output',dest='outputfile',help='output file',required=True)
 	args = argpar.parse_args()
 
 	posDict = {}
-
+	
+	featvec = extractFeatures(args.infile)
 	#outstream = open(args.outputfile,'w')
-
 	counter=0
-	for sid, sentence in enumerate(sentences(codecs.open(args.gold,encoding='utf-8'))):
+	for sid, sentence in enumerate(sentences(codecs.open(args.infile,encoding='utf-8'))):
 		for tid,token in enumerate(sentence):
-			posDict[counter] = []
-			posDict[counter].append(token.pos)
+			posDict[counter] = [token.gold_pos, token.predicted_pos]
+			if tid == 0:
+				token.createFeatureVector(featvec, sentence[tid], None, sentence[tid+1])
+			elif tid == len(sentence)-1:
+				token.createFeatureVector(featvec, sentence[tid], sentence[tid-1], None)
+			else:
+				token.createFeatureVector(featvec, sentence[tid], sentence[tid-1], sentence[tid+1])
+			#print token.sparseFeatvec
 			counter+=1
-	counter=0
-	for sid, sentence in enumerate(sentences(codecs.open(args.prediction,encoding='utf-8'))):
-		for tid,token in enumerate(sentence):
-			posDict[counter].append(token.pos)
-			counter+=1
-
 	evaluate(posDict)
 	t1 = time.time()
 	print "\n"
