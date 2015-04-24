d7fc29fb6e0c449617cf6aae1025fd5b718d8b70
YNedderhoff <yanicknedderhoff@gmail.com>
2015-04-24 14:43:56 +0200

    Handle wrong formatted input, added comments, some cosmetics

--- modules/token.py
+++ modules/token.py
@@ -1,23 +1,37 @@
 class Token(object):
 	def __init__( self, line ):
+
+		# Splits line tab-wise, writes the values in parameters
+
 		entries = line.split('\t')
-		self.form = entries[0].lower()
-		self.gold_pos = entries[1]
-		self.predicted_pos = entries [3]
+		if len(entries) == 4:
+			self.form = entries[0].lower()
+			self.gold_pos = entries[1]
+			self.predicted_pos = entries [3]
+
+		elif len(entries) > 4: print "\tInput file not in expected format: Too many columns"
+		else: print "\tInput file not in expected format: Too many columns"
 
 	def createFeatureVector(self, featvec, currentToken, previousToken, nextToken):
+
+		# creates a sparse representation of the feature vector (featvec)
+
 		self.sparseFeatvec = {}
 		
-		#if previousToken: self.sparseFeatvec[featvec["prev_pos_"+str(previousToken.gold_pos)]] = 1
-		
+		# The current token
 		self.sparseFeatvec[featvec["current_form_"+currentToken.form]] = 1
-		if previousToken: self.sparseFeatvec[featvec["prev_form_"+previousToken.form]] = 1
-		if nextToken: self.sparseFeatvec[featvec["next_form_"+nextToken.form]] = 1
-		
 
-		if not previousToken: self.sparseFeatvec[featvec["initial_token"]] = 1
+		# If exists, the previous token; else it is the initial token of the phrase
+		if previousToken: self.sparseFeatvec[featvec["prev_form_"+previousToken.form]] = 1
+		else: self.sparseFeatvec[featvec["initial_token"]] = 1
+
+		# if exists, the next token
+		if nextToken: self.sparseFeatvec[featvec["next_form_"+nextToken.form]] = 1
 
 def sentences( filestream ):
+
+	# A generator to read a file sentence-wise and generate a Token object for each line
+	# A list of Token objects of every sentence is yielded
 
 	sentence = []
 	for line in filestream:
