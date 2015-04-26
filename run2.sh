f566be5ee4d69d1acfe5d1610a62b03b9d219293
YNedderhoff <yanicknedderhoff@gmail.com>
2015-04-26 14:45:34 +0200

    Changes on run2.sh

--- run2.sh
+++ run2.sh
@@ -5,7 +5,7 @@ CORPORA="../team-lab-ss2015/data/pos"
 #paste $CORPORA/dev.col $CORPORA/dev-predicted.col >> $CORPORA/train.col
 
 # Finding possible features, e.g. Affixes.
-python perceptron.py -feat -i $CORPORA/djaneeque
+#python perceptron.py -feat -i $CORPORA/djaneeque
 
 #  Train the model
 python perceptron.py -train -i $CORPORA/djaneeque
@@ -14,6 +14,6 @@ python perceptron.py -train -i $CORPORA/djaneeque
 #python perceptron.py -test -i $CORPORA/train.col
 
 # Evaluate the results
-python perceptron.py -ev -i $CORPORA/djaneeque -o evaluation2.txt
+#python perceptron.py -ev -i $CORPORA/djaneeque -o evaluation2.txt
 
 #rm $CORPORA/train.col
