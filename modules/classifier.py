9d9394398c0437f1707513a05b8c139a01e3d1aa
wittmamz <moritzwittmann@gmail.com>
2015-04-27 14:50:32 +0200

    Added classifier class

---
+++ modules/classifier.py
@@ -0,0 +1,25 @@
+class classifier(object):
+	def __init__(self, tag, dimensions):
+		self.tag = tag
+		self.dimensions = dimensions
+		self.weight_vector = [0.0 for ind in range(dimensions)]
+
+
+
+	
+	def classify(self, feat_vec):
+		return sum([self.weight_vector[i]*float(feat_vec[i]) for i in len(feat_vec)])
+
+
+
+
+	def adjust_weights(self, feat_vec, prediction, step_size):
+		if prediction:
+			for ind in range(len(feat_vec)):
+				self.weight_vector[ind] = self.weight_vector[ind] + step_size*float(feat_vec[ind])
+		else:
+			for ind in range(len(feat_vec)):
+				self.weight_vector[ind] = self.weight_vector[ind] - step_size*float(feat_vec[ind])
+
+
+
