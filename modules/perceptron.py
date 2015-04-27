6811b67a431dcd242c3f95317f20bd66fa67a86c
wittmamz <moritzwittmann@gmail.com>
2015-04-28 00:50:31 +0200

    error investigation

--- modules/perceptron.py
+++ modules/perceptron.py
@@ -6,26 +6,26 @@ class classifier(object):
 
         # classify a token according to its feature vector:
 	def classify(self, feat_vec):
-		#return sum([self.weight_vector[i]*float(feat_vec[i]) for i in range(len(feat_vec))])
-                return sum([self.weight_vector[i] for i in feat_vec])
+		return sum([self.weight_vector[i]*float(feat_vec[i]) for i in range(len(feat_vec))])
+                #return sum([self.weight_vector[i] for i in feat_vec])
                 
         # adjust the weights upon incorrect prediction:
         #     prediction=True  -> this classifier should have tagged the token
         #     prediction=False -> this classifier incorrectly tagged the token
 	def adjust_weights(self, feat_vec, prediction, step_size):
-##		if prediction:
-##			for ind in range(len(feat_vec)):
-##				self.weight_vector[ind] = self.weight_vector[ind] + \
-##                                                          step_size*float(feat_vec[ind])
-##		else:
-##			for ind in range(len(feat_vec)):
-##				self.weight_vector[ind] = self.weight_vector[ind] - \
-##                                                          step_size*float(feat_vec[ind])
-                if prediction:
-			for ind in feat_vec:
+		if prediction:
+			for ind in range(len(feat_vec)):
 				self.weight_vector[ind] = self.weight_vector[ind] + \
-                                                          step_size*1.0
+                                                          step_size*float(feat_vec[ind])
 		else:
-			for ind in feat_vec:
+			for ind in range(len(feat_vec)):
 				self.weight_vector[ind] = self.weight_vector[ind] - \
-                                                          step_size*1.0
+                                                          step_size*float(feat_vec[ind])
+##                if prediction:
+##			for ind in feat_vec:
+##				self.weight_vector[ind] = self.weight_vector[ind] + \
+##                                                          step_size*1.0
+##		else:
+##			for ind in feat_vec:
+##				self.weight_vector[ind] = self.weight_vector[ind] - \
+##                                                          step_size*1.0
